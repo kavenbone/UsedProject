3935fad283887cc25f1999add27a42fa60a7b00c
kavenbone <kavenbone@aliyun.com>
2018-11-07 23:19:29 +0800

    Create AAAReadme.java

---
+++ websocket/AAAReadme.java
@@ -0,0 +1,42 @@
+package com.example.websocket.util;
+
+import org.java_websocket.WebSocket;
+
+import android.util.Log;
+
+public class AAAReadme {
+	//可以,使用3实现
+	//https://blog.csdn.net/android_fcp/article/details/82983236
+	//下载jar;https://mvnrepository.com/artifact/org.java-websocket/Java-WebSocket
+/*
+<uses-permission android:name="android.permission.INTERNET"></uses-permission> 
+WebSocketUtil开始
+ServerManager.start,stop
+ServerSocket.onConn,onMessage
+
+UserLeave
+SendMessageToUser
+是常见发消息功能,回消息有用
+
+ 
+ 
+ 
+*/
+
+	/*
+	命令例子:
+	注意:第三个参数为空时必须是空格
+	读取根目录
+	read_file:: 
+	创建文件夹:
+	create_folder:test: 
+	创建文件:
+	create_file:test/test.txt: 
+	写入文件
+	write_file:test/test.txt:aaaa
+	读取目录
+	read_file:test: 
+	读取文件
+	read_file:test/test.txt: 
+	 */
+}
