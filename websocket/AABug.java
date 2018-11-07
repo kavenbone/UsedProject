50622731b8aa4b1f69f79d7fa64012f8d788b785
kavenbone <kavenbone@aliyun.com>
2018-11-07 23:19:12 +0800

    Create AABug.java

---
+++ websocket/AABug.java
@@ -0,0 +1,16 @@
+package com.example.websocket.util;
+
+import org.java_websocket.WebSocket;
+
+import android.util.Log;
+
+public class AABUG {
+/*
+保存文件失败,因为result.length>3,内容有时是有":"分融符的
+读取文件失败:同上
+html:也有这个问题
+
+
+
+ */
+}
